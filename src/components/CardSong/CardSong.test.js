1dc029edac851d03a3e6a254be21e33bf7fb4934
baiest <ballesteros.juan@correounivalle.edu.co>
2022-04-04 19:16:08 -0500

    Update test

--- src/components/CardSong/CardSong.test.js
+++ src/components/CardSong/CardSong.test.js
@@ -3,7 +3,7 @@ import React from "react"
 import renderer from "react-test-renderer"
 import { CardSong } from "."
 describe("CardSong", () => {
-  const tree = renderer.create( < CardSong / > ).toJSON()
+  const tree = renderer.create( < CardSong /> ).toJSON()
   it("renders correctly", () => {
     expect(tree).toMatchSnapshot()
   })
