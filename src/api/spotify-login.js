0cdc99bf492996db9d570690d1d2b319732b6f0e
baiest <ballesteros.juan@correounivalle.edu.co>
2022-04-07 14:33:28 -0500

    :hammer: Fix api function

--- src/api/spotify-login.js
+++ src/api/spotify-login.js
@@ -1,24 +1,26 @@
 const SpotifyWebApi = require("spotify-web-api-node")
 export default async function spotifyLoginHandler(req, res) {
-  // res.setHeader(`Cache-Control`, `public, max-age=60`);
-  // const code = req.body.code
-  // const spotifyApi = new SpotifyWebApi({
-  //   redirectUri: `${process.env.REDIRECT_URI}/login`,
-  //   clientId: process.env.CLIENT_ID,
-  //   clientSecret: process.env.CLIENT_SECRET,
-  // })
-  // return spotifyApi
-  //   .authorizationCodeGrant(code)
-  //   .then(data => {
-  //     return res.json({
-  //       access_token: data.body.access_token,
-  //       refresh_token: data.body.refresh_token,
-  //       expires_in: data.body.expires_in,
-  //     })
-  //   })
-  //   .catch(error => {
-  //     console.log(error)
-  //     return res.sendStatus(401)
-  //   })
-  res.json({ hola: "mundo" })
+  try {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: `${process.env.REDIRECT_URI}/login`,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    })
+    return spotifyApi
+      .authorizationCodeGrant(code)
+      .then(data => {
+        return res.json({
+          access_token: data.body.access_token,
+          refresh_token: data.body.refresh_token,
+          expires_in: data.body.expires_in,
+        })
+      })
+      .catch(error => {
+        console.log(error)
+        return res.sendStatus(401)
+      })
+  } catch (error) {
+    res.json({ error: error.message })
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { ListSongs } from "../components/ListSongs"
 import { Track } from "../providers/Track"
-
+import Seo from "../components/seo"
 const IndexPage = () => {
   const getData = React.useCallback(() => Track.getTracks(), [])
   React.useEffect(() => console.log(process.env.REDIRECT_URI), [])
   return (
     <>
+      <Seo title="Home" />
       <h2>Vienvenido a MySpotify</h2>
       <ListSongs getData={getData} />
     </>
