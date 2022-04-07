bc3fd00248e63da51d2302e48ac63988ed848056
baiest <ballesteros.juan@correounivalle.edu.co>
2022-04-07 16:03:50 -0500

    Test gatsby env

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Track } from "../providers/Track"
 import Seo from "../components/seo"
 const IndexPage = () => {
   const getData = React.useCallback(() => Track.getTracks(), [])
+  React.useEffect(() => console.log(process.env.GATSBY_REDIRECT_URI), [])
   return (
     <>
       <Seo title="Home" />
