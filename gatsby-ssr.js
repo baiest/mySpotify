8c8cef95eddaa2c1d33c2853fab5566c4b4348dc
baiest <ballesteros.juan@correounivalle.edu.co>
2022-04-07 16:36:33 -0500

    Test env in gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,6 +10,14 @@ const { Layout } = require("./src/components/Layout")
 const { GlobalStyles } = require("./src/styles")
 const { ContextProvider } = require("./src/Context")
 const { initialState, contextReducer } = require("./src/Context/ContextReducer")
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
 
 export const wrapRootElement = ({ element }) => (
   <ContextProvider initialState={initialState} reducer={contextReducer}>
